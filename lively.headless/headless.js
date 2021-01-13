7a210794c8de3bc727ab3fec16a97200ba3ae8b5
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-01-13 16:22:27 +0100

    enter worldname

--- lively.headless/headless.js
+++ lively.headless/headless.js
@@ -4,7 +4,11 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('http://127.0.0.1:9011');
+  await page.goto('http://127.0.0.1:9011/worlds/load?name=__newWorld__');
+  await new Promise(r => { setTimeout(r, 120000)})
+  await page.keyboard.press('f');
+  await page.keyboard.press('Enter'); // Enter Key
+  await new Promise(r => { setTimeout(r, 5000)})
   await page.screenshot({path: 'example.png'});
   await browser.close();
 })();
