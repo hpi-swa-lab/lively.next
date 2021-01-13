4d69bcb55b3380d82ba298240437e4b067024a26
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-01-13 15:40:15 +0100

    switch webpage

--- lively.headless/headless.js
+++ lively.headless/headless.js
@@ -4,7 +4,7 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('localhost:9011');
+  await page.goto('https://www.google.com');
   await page.screenshot({path: 'example.png'});
   await browser.close();
 })();
